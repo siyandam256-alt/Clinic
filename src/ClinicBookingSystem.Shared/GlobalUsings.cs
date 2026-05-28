5202ee040ff985edc94560d31d3843f5ada69afa
siyandam256-alt <siyandam256@gmail.com>
2026-05-28 07:09:07 +0200

    Add GlobalUsings for shared project

---
+++ src/ClinicBookingSystem.Shared/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using System;
+global using System.Collections.Generic;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.Linq;
+global using System.Threading.Tasks;
